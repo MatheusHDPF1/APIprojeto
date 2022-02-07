d6732d41deca4ecddee9bc559e34c450131600b2
MATHEUS HENRIQUE DE PAULA FERREIRA <matheus.hpferreira@senacsp.edu.br>
2022-02-07 11:22:56 -0300

    inicio do BackEnd

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const cors = require("express");
+
+const mongoose = require("mongoose");
+
+const bcrypt = require("bcrypt");
+
+const jwt = require("jsonwebtoken");
+
+const cfn = require("./config");
+
